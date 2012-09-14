e49b9e55aa0d38107e6b637d9ca128648f98049f
gil <contato@andregil.org>
2012-09-14 11:00:40 -0300

    extracting positioning/sizing method logic to a method

--- chosen/chosen.jquery.js
+++ chosen/chosen.jquery.js
@@ -587,7 +587,6 @@ Copyright (c) 2011 by Harvest
     };
 
     Chosen.prototype.results_show = function() {
-      var dd_top, maxHeight, offset, realDropdownTop;
       if (!this.is_multiple) {
         this.selected_item.addClass("chzn-single-with-drop");
         if (this.result_single_selected) {
@@ -599,33 +598,40 @@ Copyright (c) 2011 by Harvest
         });
         return false;
       }
-      dd_top = this.is_multiple ? this.container.height() : this.container.height() - 1;
-      offset = this.container.offset();
-      this.form_field_jq.trigger("liszt:showing_dropdown", {
-        chosen: this
-      });
-      this.dropdown.css({
-        "top": (offset.top + dd_top) + "px",
-        "left": offset.left + "px",
-        "width": (this.container.outerWidth(true) - 2) + "px",
-        "maxHeight": "99999px",
-        "display": "block"
-      });
-      this.search_results.css("maxHeight", "240px");
-      realDropdownTop = this.dropdown.offset().top - $(window).scrollTop();
-      maxHeight = $(window).height() - realDropdownTop;
-      if (maxHeight > 240) {
-        maxHeight = 240;
-      }
-      if (maxHeight < 100) {
-        maxHeight = 100;
-      }
-      this.dropdown.css("maxHeight", maxHeight + "px");
-      this.search_results.css("maxHeight", (maxHeight - this.search_container.height() - 10) + "px");
       this.results_showing = true;
+      this.update_position();
       this.search_field.focus();
       this.search_field.val(this.search_field.val());
       return this.winnow_results();
+    };
+
+    Chosen.prototype.update_position = function() {
+      var dd_top, maxHeight, offset, realDropdownTop;
+      if (this.results_showing) {
+        dd_top = this.is_multiple ? this.container.height() : this.container.height() - 1;
+        offset = this.container.offset();
+        this.form_field_jq.trigger("liszt:showing_dropdown", {
+          chosen: this
+        });
+        this.dropdown.css({
+          "top": (offset.top + dd_top) + "px",
+          "left": offset.left + "px",
+          "width": (this.container.outerWidth(true) - 2) + "px",
+          "maxHeight": "99999px",
+          "display": "block"
+        });
+        this.search_results.css("maxHeight", "240px");
+        realDropdownTop = this.dropdown.offset().top - $(window).scrollTop();
+        maxHeight = $(window).height() - realDropdownTop;
+        if (maxHeight > 240) {
+          maxHeight = 240;
+        }
+        if (maxHeight < 100) {
+          maxHeight = 100;
+        }
+        this.dropdown.css("maxHeight", maxHeight + "px");
+        return this.search_results.css("maxHeight", (maxHeight - this.search_container.height() - 10) + "px");
+      }
     };
 
     Chosen.prototype.results_hide = function() {
--- coffee/chosen.jquery.coffee
+++ coffee/chosen.jquery.coffee
@@ -236,33 +236,36 @@ class Chosen extends AbstractChosen
       @form_field_jq.trigger("liszt:maxselected", {chosen: this})
       return false
 
-    dd_top = if @is_multiple then @container.height() else (@container.height() - 1)
-    offset = @container.offset()
-    @form_field_jq.trigger("liszt:showing_dropdown", {chosen: this})
-    @dropdown.css {
-      "top": (offset.top + dd_top) + "px",
-      "left": offset.left + "px",
-      "width": (@container.outerWidth(true) - 2) + "px", # 2px of border
-      "maxHeight": "99999px",
-      "display": "block"
-    }
-
-    @search_results.css("maxHeight", "240px")
-
-    # Fix maximum size
-    realDropdownTop = @dropdown.offset().top - $(window).scrollTop()
-    maxHeight = $(window).height() - realDropdownTop
-    maxHeight = 240 if maxHeight > 240
-    maxHeight = 100 if maxHeight < 100
-    @dropdown.css("maxHeight", maxHeight + "px")
-    @search_results.css("maxHeight", ( maxHeight - @search_container.height() - 10 ) + "px")
-
     @results_showing = true
 
+    @update_position()
     @search_field.focus()
     @search_field.val @search_field.val()
 
     this.winnow_results()
+
+  update_position: ->
+    if @results_showing
+      dd_top = if @is_multiple then @container.height() else (@container.height() - 1)
+      offset = @container.offset()
+      @form_field_jq.trigger("liszt:showing_dropdown", {chosen: this})
+      @dropdown.css {
+        "top": (offset.top + dd_top) + "px",
+        "left": offset.left + "px",
+        "width": (@container.outerWidth(true) - 2) + "px", # 2px of border
+        "maxHeight": "99999px",
+        "display": "block"
+      }
+
+      @search_results.css("maxHeight", "240px")
+
+      # Fix maximum size
+      realDropdownTop = @dropdown.offset().top - $(window).scrollTop()
+      maxHeight = $(window).height() - realDropdownTop
+      maxHeight = 240 if maxHeight > 240
+      maxHeight = 100 if maxHeight < 100
+      @dropdown.css("maxHeight", maxHeight + "px")
+      @search_results.css("maxHeight", ( maxHeight - @search_container.height() - 10 ) + "px")
 
   results_hide: ->
     @selected_item.removeClass "chzn-single-with-drop" unless @is_multiple
